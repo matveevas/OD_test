144a97b47797c52a60539aaf4d9a60925d3718a6
svetlana.matveeva <matveevas94@yandex.ru>
2018-06-12 17:06:29 +0300

    add geospark object

--- src/main/java/org/grint/masterthesis/testscala210/main/GeoSpark.scala
+++ src/main/java/org/grint/masterthesis/testscala210/main/GeoSpark.scala
@@ -1,5 +1,6 @@
 package org.grint.masterthesis.testscala210.main
 
+
 import com.vividsolutions.jts.geom.Geometry
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.api.java.JavaRDD
@@ -7,12 +8,13 @@ import org.apache.spark.sql.SparkSession
 import org.datasyslab.geospark.enums.{FileDataSplitter, GridType, IndexType}
 import org.grint.masterthesis.testscala210.loader.DataLoader
 import org.datasyslab.geospark.spatialRDD.{PointRDD, PolygonRDD, SpatialRDD}
-import org.datasyslab.geospark.spatialPartitioning
-import org.datasyslab.geosparksql.utils.Adapter
+import org.datasyslab.geospark.{enums, spatialPartitioning}
+//import org.datasyslab.geosparksql.utils.Adapter
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.datasyslab.geospark.spatialOperator.JoinQuery
 import org.datasyslab.geospark.spatialOperator.JoinQuery.DistanceJoinQuery
 import org.datasyslab.geospark.spatialOperator.JoinQuery.DistanceJoinQueryCountByKey
+import org.datasyslab.geospark.spatialRDD.PointRDD._
 
 
 
@@ -29,12 +31,25 @@ object GeoSpark {
 
     val sc = new SparkContext(conf)
 
-//    val pointRDDInputLocation = DataLoader.cardsDF.col("applicantlocation")
-//    val pointRDDOffset = 0 // The WKT string starts from Column 0
-//    val pointRDDSplitter = FileDataSplitter.GEOJSON
-//    val carryOtherAttributes = true
-//    var objectRDD = new PointRDD(sparkSession, pointRDDInputLocation, pointRDDOffset, pointRDDSplitter, carryOtherAttributes)
-//    var gsDF = DataLoader.cardsDF.select("applicantlocation")
+   val InputLocation = "svetlana.matveeva/Documents/MasterThesis/arealmsmall.csv"//DataLoader.cardsDF.col("applicantlocation")
+    val Offset = 0 // The WKT string starts from Column 0
+    val Splitter = FileDataSplitter.CSV//GEOJSON
+    val gridType = GridType.KDBTREE
+    val numPartitions = 200
+    val carryOtherAttributes = true
+
+   var pointRDD: PointRDD = new PointRDD (sc,InputLocation,Offset,Splitter, carryOtherAttributes, numPartitions)
+    //val res = spatialRDD.spatialPartitionedRDD(GridType.KDBTREE)
+    val res2 = pointRDD.spatialPartitioning(gridType)
+   // var objectRDD = new PointRDD(sparkSession, InputLocation, Offset, Splitter, carryOtherAttributes)
+//  val inputLocation: String = HDFS_directory+"/colocation/partitio prdd ns/reducedGDELT/part-00000"
+//        val offset: Int = 2
+//        val splitter: String = "csv"
+//        val gridType: String = "X-Y"
+//        val numPartions: Int =200
+//
+//        var pointRDD: PointRDD = new PointRDD(sc, inputLocation, offset, splitter, gridType, numPartions)
+    // var gsDF = DataLoader.cardsDF.select("applicantlocation")
 //    var spatialRDD =new SpatialRDD[Geometry]
 //    spatialRDD.rawSpatialRDD =Adapter.toRdd(gsDF)
 //
@@ -51,32 +66,33 @@ object GeoSpark {
 //   // gsDF.take(10).foreach(println)
 //val resourceFolder = System.getProperty("user.dir") + "/src/test/resources/"
 
-    val PointRDDInputLocation = "svetlana.matveeva/Documents/MasterThesis/arealmsmall.csv"
-    val PointRDDSplitter = FileDataSplitter.CSV
-    val PointRDDIndexType = IndexType.RTREE
-    val PointRDDNumPartitions = 5
-    val PointRDDOffset = 1
-
-    val PolygonRDDInputLocation = "svetlana.matveeva/Documents/MasterThesis/primaryroads-polygon.csv"
-    val PolygonRDDSplitter = FileDataSplitter.CSV
-    val PolygonRDDIndexType = IndexType.RTREE
-    val PolygonRDDNumPartitions = 5
-    val PolygonRDDStartOffset = 0
-    val PolygonRDDEndOffset = 9
-
-    val queryWindowRDD = new PolygonRDD(sc, PolygonRDDInputLocation, PolygonRDDStartOffset, PolygonRDDEndOffset, PolygonRDDSplitter, true)
-    val objectRDD = new PointRDD(sc, PointRDDInputLocation, PointRDDOffset, PointRDDSplitter, false)
-    objectRDD.analyze()
-    objectRDD.spatialPartitioning(GridType.QUADTREE)
-    queryWindowRDD.spatialPartitioning(objectRDD.getPartitioner)
-    val eachQueryLoopTimes = 1
-    for (i <- 1 to eachQueryLoopTimes) {
-     //val resultSize = JoinQuery.DistanceJoinQuery(objectRDD, queryWindowRDD, false, true).count()
-      //val res2 = JoinQuery.distanceJoin(objectRDD, queryWindowRDD, false, true).count()
-    }
-
-    //val resultSize = JoinQuery.DistanceJoinQuery(objectRDD, queryWindowRDD, false, true).count()
-    //val res2= JoinQuery.distanceJoin(objectRDD, queryWindowRDD, false, true).count()
-
+    // try use example from https://github.com/DataSystemsLab/GeoSpark/blob/master/core/src/test/scala/org/datasyslab/geospark/scalaTest.scala#L38
+//    val PointRDDInputLocation = "svetlana.matveeva/Documents/MasterThesis/arealmsmall.csv"
+//    val PointRDDSplitter = FileDataSplitter.CSV
+//    val PointRDDIndexType = IndexType.RTREE
+//    val PointRDDNumPartitions = 5
+//    val PointRDDOffset = 1
+//
+//    val PolygonRDDInputLocation = "svetlana.matveeva/Documents/MasterThesis/primaryroads-polygon.csv"
+//    val PolygonRDDSplitter = FileDataSplitter.CSV
+//    val PolygonRDDIndexType = IndexType.RTREE
+//    val PolygonRDDNumPartitions = 5
+//    val PolygonRDDStartOffset = 0
+//    val PolygonRDDEndOffset = 9
+//
+//    val queryWindowRDD = new PolygonRDD(sc, PolygonRDDInputLocation, PolygonRDDStartOffset, PolygonRDDEndOffset, PolygonRDDSplitter, true)
+//    val objectRDD = new PointRDD(sc, PointRDDInputLocation, PointRDDOffset, PointRDDSplitter, false)
+//    objectRDD.analyze()
+//    objectRDD.spatialPartitioning(GridType.QUADTREE)
+//    queryWindowRDD.spatialPartitioning(objectRDD.getPartitioner)
+//    val eachQueryLoopTimes = 1
+//    for (i <- 1 to eachQueryLoopTimes) {
+// val resultSize = JoinQuery.DistanceJoinQuery(objectRDD, queryWindowRDD, false, true).count()
+//      //val res2 = JoinQuery.distanceJoin(objectRDD, queryWindowRDD, false, true).count()
+//    }
+//
+//    // val resultSize = JoinQuery.DistanceJoinQuery(objectRDD, queryWindowRDD, false, true).count()
+//    //val res2= JoinQuery.distanceJoin(objectRDD, queryWindowRDD, false, true).count()
+//
   }
 }
